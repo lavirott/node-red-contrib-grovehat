e7dd4c92298ac81c82c9bdf2ce0f967c5f8d351f
Stéphane Lavirotte <stephane@lavirotte.com>
2019-06-16 15:13:52 +0200

    dos2unix on files

--- grove_4_digit_display.js
+++ grove_4_digit_display.js
@@ -1,12 +1,12 @@
 module.exports = function (RED) {
-	const Grove4DigitDisplay = require('node-grovehat');
-	
+	const Grove4DigitDisplay = require('node-grovehat').Grove4DigitDisplay;
+
 	function Grove4DigitDisplayNode(config) {
 		RED.nodes.createNode(this, config);
 		this.pin = parseInt(config.pin) || 5;
 
 		if (RED.settings.verbose) { this.log("Grove4DigitDisplay: Pin: " + this.pin); }
-		
+
 		var display = new Grove4DigitDisplay(this.pin, this.pin + 1);
 
 		var node = this;
