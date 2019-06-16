b69ac6f57d0c59c8a29d652442486cb74c9b1932
Stéphane Lavirotte <stephane@lavirotte.com>
2019-06-16 16:32:15 +0200

    Correct bug on chainable rgb led, add doc, parse rgb and hsv messages

--- grove_chainable_rgb_led.js
+++ grove_chainable_rgb_led.js
@@ -1,18 +1,27 @@
 module.exports = function (RED) {
 	const GroveChainableRGBLed = require('node-grovehat').GroveChainableRGBLed;
-	
+
 	function GroveChainableRGBLedNode(config) {
 		RED.nodes.createNode(this, config);
 		this.pin = parseInt(config.pin) || 5;
 
 		if (RED.settings.verbose) { this.log("GroveChainableRGBLed: Pin: " + this.pin); }
-		
-		var display = new GroveChainableRGBLed(this.pin, this.pin + 1);
+
+		var led = new GroveChainableRGBLed(this.pin, this.pin + 1);
 
 		var node = this;
 		node.on('input', function (msg) {
-			display.show(msg.payload);
+			// msg.payload is rgb(red, green, blue)
+			// format is ['rgb', 'red, green, blue']
+			var format = msg.payload.replace(')', '').split('(');
+			values = format[1].split(",");
+			if (format[0] == "rgb") {
+				led.setColorRGB(0, values[0], values[1], values[2]);
+			} else if (format[0] == "hsv") {
+				led.setColorHSB(0, values[0] / 360.0, 1.0, 0.5);
+				//led.setColorHSB(0, values[0] / 360.0, parseInt(values[1]) / 100.0, parseInt(values[2]) / 100.0);
+			}
 		});
 	}
-	RED.nodes.registerType("grove-chainable_rgb_led", GroveChainableRGBLedNode);
+	RED.nodes.registerType("grove-chainable-rgb-led", GroveChainableRGBLedNode);
 }
